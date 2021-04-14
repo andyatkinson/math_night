eee025b283d4a92020f7672876abcc44eec506c5
Andy Atkinson <andyatkinson@gmail.com>
2021-04-14 18:42:03 -0500

    Add some instructions

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,3 +1,9 @@
+# In a Terminal, type "irb" to load interactive Ruby
+#
+# Paste this code in, and call the "rock_paper_scissors" method
+#
+# Try to beat the computer!
+#
 def rock_paper_scissors
   puts "1"; sleep(1)
   puts "2"; sleep(1)
